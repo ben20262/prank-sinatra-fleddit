fbe3370c605a7a7f32aac278e5ff9dbe5ebdd2ba
ben20262 <ben20262@aim.com>
2020-02-11 11:48:35 -0500

    post controller build

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,34 @@
 class PostsController < AppController
 
-    get '/posts/:id' do
-        if !logged_in?
-            redirect '/login'
-        end
-        post = Post.find(params[:id])
-        erb :'posts/show'
-    end
-
     get '/pages/:page_id/posts/new' do
         if !logged_in?
             redirect '/login'
-        elsif !UserPage.find(current_user.id)
+        elsif !UserPage.find_by(user_id: current_user.id, page_id: params[:page_id].to_i)
+            redirect "/pages/#{params[:page_id]}/join"
         end
-        @page = Page.find(params[:id])
+        @page = Page.find(params[:page_id])
         erb :'posts/new'
     end
 
-    post '/pages/:page_id/posts' do
+    get '/pages/:page_id/posts/:post_id' do
+        @page = Page.find(params[:page_id])
+        @post = Post.find(params[:post_id])
+        if !logged_in?
+            redirect '/login'
+        elsif !UserPage.find_by(user_id: current_user.id, page_id: params[:page_id].to_i)
+            redirect "/pages/#{params[:page_id]}/join"
+        end
+        post = Post.find(params[:post_id])
+        erb :'posts/show'
+    end
 
+    post '/pages/:page_id/posts' do
+        post = Post.new(title: params[:title], content: params[:content], page_id: params[:page_id], user_id: current_user.id)
+
+        if post.save
+            redirect "/pages/#{post.page_id}/posts/#{post.id}"
+        end
+        redirect "/pages/#{params[:page_id]}/posts/new"
     end
 
     get '/pages/:page_id/posts/:post_id/edit' do
@@ -30,6 +40,17 @@ class PostsController < AppController
             redirect "/pages/#{params[:page_id]}"
         end
         erb :'posts/edit'
+    end
+
+    get '/pages/:page_id/posts/:post_id/delete' do
+        @page = Page.find(params[:page_id])
+        @post = Post.find(params[:post_id])
+        if !logged_in?
+            redirect '/login'
+        elsif @post.user_id != current_user.id || !UserPage.find_by(page_id: @page.id, user_id: current_user.id)
+            redirect "/pages/#{params[:page_id]}"
+        end
+        erb :'posts/delete'
     end
 
     patch '/pages/:page_id/posts/:post_id' do
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
     validates_presence_of :content, :user_id, :page_id
-    belongs_to :pages
-    belongs_to :users
+    belongs_to :page
+    belongs_to :user
 end
