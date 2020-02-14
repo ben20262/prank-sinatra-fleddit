a60ff2dc0f78fac5d1207d429f5722bf843062ea
ben20262 <ben20262@aim.com>
2020-02-14 15:20:44 -0500

    uniqueness requirement added

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,6 @@
 class Page < ActiveRecord::Base
     validates_presence_of :name, :about
+    validates_uniqueness_of :name
     has_many :posts
     has_many :user_pages
     has_many :users, through: :user_pages
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     validates_presence_of :username, :email
+    validates_uniqueness_of :username
     has_secure_password
     has_many :posts
     has_many :user_pages
